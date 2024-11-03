e29565ef96cb52333c27dfd10e2c1297c1856845
tfachmann <fachmanntj@gmail.com>
2024-11-03 16:05:31 +0100

    chore: change metadata of template

--- src/main.rs
+++ src/main.rs
@@ -18,9 +18,9 @@ fn main() -> eframe::Result {
         ..Default::default()
     };
     eframe::run_native(
-        "eframe template",
+        "bewegtbild",
         native_options,
-        Box::new(|cc| Ok(Box::new(eframe_template::TemplateApp::new(cc)))),
+        Box::new(|cc| Ok(Box::new(bewegtbild::TemplateApp::new(cc)))),
     )
 }
 
